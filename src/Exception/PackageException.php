3a52b70f7dc8362c1bfb808e5371de080c2537ce
ustal <quit.ipad2@gmail.com>
2017-05-31 11:32:52 +0300

    add Exception codes

--- src/Exception/PackageException.php
+++ src/Exception/PackageException.php
@@ -4,4 +4,8 @@ namespace RapidAPI\Exception;
 
 class PackageException extends \Exception
 {
+    const URL_CODE = 1;
+    const METHOD_CODE = 2;
+    const JSON_VALIDATION_CODE = 3;
+    const BLOCK_NOT_EXIST_CODE = 4;
 }
